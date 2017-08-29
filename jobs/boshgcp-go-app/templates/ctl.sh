87f99b2a91b9946b688ab852baa3ee165ef1e355
anthonysroberts <anthonysroberts@bosh-bastion.c.asrgcp-02.internal>
2017-08-29 10:15:59 +0000

    initialised repo as a BOSH release using init-release

---
+++ jobs/boshgcp-go-app/templates/ctl.sh
@@ -0,0 +1,29 @@
+PP_DIR=/var/vcap/sys/run/boshgcp-go-app
+LOG_DIR=/var/vcap/sys/log/boshgcp-go-app
+PIDFILE=${APP_DIR}/pid
+RUN_USER=vcap
+ 
+case $1 in
+     
+     start)
+         mkdir -p $APP_DIR $LOG_DIR
+         chown -R $RUN_USER:$RUN_USER $APP_DIR $LOG_DIR
+                             
+         echo $$ > $PIDFILE
+                                     
+         cd /var/vcap/packages/boshgcp-go-app
+                                             
+         exec /var/vcap/packages/boshgcp-go-app/boshgcp-go-app \
+             >>  $LOG_DIR/boshgcp-go-app.stdout.log \
+             2>> $LOG_DIR/boshgcp-go-app.stderr.log
+                                                     
+         ;;
+                                                             
+     stop)
+         kill -9 `cat $PIDFILE` && rm -f $PIDFILE
+         ;;
+                                                          
+     *)
+         echo "Usage: ctl {start|stop}" ;;
+                                                                                                     
+     esac
